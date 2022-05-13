865410af62475b111bd0ef071c38ccc38ea9057b
Jaime Gonzalez <curlyjoe71@gmail.com>
2022-05-13 09:48:02 -0700

    Create theme

---
+++ client/style/theme.tsx
@@ -0,0 +1,55 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    // type: 'dark',
+    primary: {
+      main: '#cce656',
+    },
+    secondary: {
+      main: '#f35e78',
+    },
+  },
+  typography: {
+    h1: {
+      fontFamily: '"Montserrat", "Helvetica", "Arial", sans-serif',
+      fontWeight: 200,
+    },
+    h2: {
+      fontFamily: '"Montserrat", "Helvetica", "Arial", sans-serif',
+    },
+    h3: {
+      fontFamily: '"Montserrat", "Helvetica", "Arial", sans-serif',
+    },
+    h4: {
+      fontFamily: '"Montserrat", "Helvetica", "Arial", sans-serif',
+    },
+    h5: {
+      fontFamily: '"Montserrat", "Helvetica", "Arial", sans-serif',
+    },
+    h6: {
+      fontFamily: '"Montserrat", "Helvetica", "Arial", sans-serif',
+    },
+    body1: {
+      fontFamily: '"Mulish", "Helvetica", "Arial", sans-serif',
+    },
+    body2: {
+      fontFamily: '"Mulish", "Helvetica", "Arial", sans-serif',
+    },
+    button: {
+      fontFamily: '"Montserrat", "Helvetica", "Arial", sans-serif',
+    },
+    caption: {
+      fontFamily: '"Mulish", "Helvetica", "Arial", sans-serif',
+    },
+    subtitle1: {
+      fontFamily: '"Mulish", "Helvetica", "Arial", sans-serif',
+    },
+    subtitle2: {
+      fontFamily: '"Mulish", "Helvetica", "Arial", sans-serif',
+    },
+    fontFamily: '"Mulish", "Helvetica", "Arial", sans-serif',
+  },
+});
+
+export default theme;
